8eb27aff68ba1424c882bf1cbf3e5ec1be55539d
h.narozny <h.narozny@merixstudio.com>
2018-05-29 14:44:34 +0200

    New: Laravel Service provider and facade

---
+++ src/Integration/Laravel/JwPlatformServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace HubertNNN\JwPlatform\Integration\Laravel;
+
+use HubertNNN\JwPlatform\Contracts;
+use HubertNNN\JwPlatform\JwPlatformService;
+use Illuminate\Config\Repository;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+
+class JwPlatformServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config.php', 'jwplatform'
+        );
+
+        $this->app->singleton(Contracts\JwPlatformService::class, function ($app) {
+
+            /** @var Application $app */
+            /** @var Repository $config */
+            $config = $app['config'];
+
+            $apiKey = $config->get('jwplatform.credentials.apiKey');
+            $secret = $config->get('jwplatform.credentials.secret');
+
+            return new JwPlatformService($apiKey, $secret);
+        });
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config.php' => config_path('jwplatform.php')
+        ], 'config');
+    }
+}
